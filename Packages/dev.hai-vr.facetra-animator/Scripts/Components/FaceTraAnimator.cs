d86df7753b9ac393453ec24cdc179458edb73579
Haï~ <hai-vr@users.noreply.github.com>
2023-11-13 04:33:36 +0100

    interpolator

--- Packages/dev.hai-vr.facetra-animator/Scripts/Components/FaceTraAnimator.cs
+++ Packages/dev.hai-vr.facetra-animator/Scripts/Components/FaceTraAnimator.cs
@@ -21,8 +21,12 @@ namespace FaceTraAnimator.Runtime
             var ctrl = aac.NewAnimatorController();
             var fx = ctrl.NewLayer();
 
-            var tree = MegaTree.NewMegaTree(aac, fx)
-                .BinaryDecoder(fx.FloatParameter("OUTPUT"), "Prefix_", 4)
+            var interpolationAmount = fx.FloatParameter("AMOUNT");
+            fx.OverrideValue(interpolationAmount, 0.8f);
+            
+            var tree = MegaTree.NewMegaTree(aac, fx, fx.FloatParameter("ONE"))
+                .BinaryDecoder(fx.FloatParameter("DECODED"), "Prefix_", 4)
+                .Interpolator(fx.FloatParameter("SMOOTHED"), fx.FloatParameter("DECODED"), interpolationAmount, MegaTree.InterpolatorRange.Joystick)
                 .Tree;
 
             fx.NewState("Direct").WithAnimation(tree);
@@ -36,19 +40,22 @@ namespace FaceTraAnimator.Runtime
         private readonly AacFlBase aac;
         private readonly AacFlBlendTreeDirect _direct;
         private readonly AacFlLayer _layer;
+        private readonly AacFlFloatParameter _one;
 
         public AacFlBlendTreeDirect Tree => _direct;
 
-        public static MegaTree NewMegaTree(AacFlBase aac, AacFlLayer layer)
+        public static MegaTree NewMegaTree(AacFlBase aac, AacFlLayer layer, AacFlFloatParameter one)
         {
-            return new MegaTree(aac, aac.NewBlendTree().Direct(), layer);
+            layer.OverrideValue(one, 1f);
+            return new MegaTree(aac, aac.NewBlendTree().Direct(), layer, one);
         }
 
-        private MegaTree(AacFlBase aac, AacFlBlendTreeDirect direct, AacFlLayer layer)
+        private MegaTree(AacFlBase aac, AacFlBlendTreeDirect direct, AacFlLayer layer, AacFlFloatParameter one)
         {
             this.aac = aac;
             _direct = direct;
             _layer = layer;
+            _one = one;
         }
 
         public MegaTree BinaryDecoder(AacFlFloatParameter output, string prefix, int totalBits)
@@ -74,6 +81,37 @@ namespace FaceTraAnimator.Runtime
         private static int BitValueContribution(int index)
         {
             return (int)Math.Pow(2, index);
+        }
+
+        public enum InterpolatorRange
+        {
+            Joystick,
+            Analog
+        }
+        
+        public MegaTree Interpolator(AacFlFloatParameter smoothed, AacFlFloatParameter wanted, AacFlFloatParameter amount, InterpolatorRange range)
+        {
+            var lower = range == InterpolatorRange.Joystick ? -1f : 0f;
+            var upper = 1f;
+            return Interpolator(smoothed, wanted, amount, lower, upper);
+        }
+
+        private MegaTree Interpolator(AacFlFloatParameter smoothed, AacFlFloatParameter wanted, AacFlFloatParameter amount, float lower, float upper)
+        {
+            var lowerAnim = AAP(smoothed, lower);
+            var upperAnim = AAP(smoothed, upper);
+            var interpolator = aac.NewBlendTree().Simple1D(amount)
+                .WithAnimation(aac.NewBlendTree().Simple1D(wanted)
+                        .WithAnimation(lowerAnim, lower)
+                        .WithAnimation(upperAnim, upper),
+                    0)
+                .WithAnimation(aac.NewBlendTree().Simple1D(smoothed)
+                        .WithAnimation(lowerAnim, lower)
+                        .WithAnimation(upperAnim, upper),
+                    1);
+            _direct.WithAnimation(interpolator, _one);
+
+            return this;
         }
 
         private AacFlClip AAP(AacFlFloatParameter output, float value)
